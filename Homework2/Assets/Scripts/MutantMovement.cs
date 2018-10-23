4dd58808dff569e25616474818d2e8921de80b0e
Vrezh Khalatyan <vkhalat@calstatela.edu>
2018-10-23 16:28:59 -0700

    clean up merge conflicts on MutantMovement

--- Homework2/Assets/Scripts/MutantMovement.cs
+++ Homework2/Assets/Scripts/MutantMovement.cs
@@ -22,7 +22,7 @@ public class MutantMovement : MonoBehaviour {
 
 			this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
 
-<<<<<<< HEAD
+
             anim.SetBool("isIdle", false);
             if(direction.magnitude > 5){
                 this.transform.Translate(0, 0, 0.05f);
@@ -65,47 +65,5 @@ public class MutantMovement : MonoBehaviour {
             anim.SetBool("isRunning", false);
             anim.SetBool("isJumping", false);
         }
-=======
-			anim.SetBool("isIdle", false);
-			if(direction.magnitude > 5){
-				this.transform.Translate(0, 0, 0.05f);
-				anim.SetBool("isWalking", true);
-				anim.SetBool("isAttacking", false);
-				anim.SetBool("isRunning", false);
-				anim.SetBool("isJumping", false);
-				anim.SetBool("isIdle", false);
-				if(direction.magnitude > 6){
-					this.transform.Translate(0, 0, 0.07f);
-					anim.SetBool("isRunning", true);
-					anim.SetBool("isWalking", false);
-					anim.SetBool("isAttacking", false);
-					anim.SetBool("isJumping", false);
-					anim.SetBool("isIdle", false);
-					if(direction.magnitude > 8){
-						this.transform.Translate(0,0,0.01f);
-						anim.SetBool("isRunning", false);
-						anim.SetBool("isWalking", false);
-						anim.SetBool("isAttacking", false);
-						anim.SetBool("isJumping", true);
-						anim.SetBool("isIdle", false);
-					}
-				}
-			}else{
-				anim.SetBool("isAttacking", true);
-				this.transform.Translate(0,0,0.01f);
-				_MS.enabled = true;
-				anim.SetBool("isWalking", false);
-				anim.SetBool("isRunning", false);
-				anim.SetBool("isJumping", false);
-				anim.SetBool("isIdle", false);
-			}
-		}else{
-			anim.SetBool("isIdle", true);
-			anim.SetBool("isWalking", false);
-			anim.SetBool("isAttacking", false);
-			anim.SetBool("isRunning", false);
-			anim.SetBool("isJumping", false);
-		}
->>>>>>> 600db938947957cda41c4ae4d5a54b1d1612926b
 	}
 }
