277975e19501b5a57388e1505b3367a3303b0b0b
Russell Carter <r.cartercoding@gmail.com>
2018-10-23 22:15:15 -0700

    bystander now runs to target then toggles to idle animation

--- Homework2/Assets/Scripts/BystanderMovement.cs
+++ Homework2/Assets/Scripts/BystanderMovement.cs
@@ -7,6 +7,7 @@ public class BystanderMovement : MonoBehaviour {
     [SerializeField] private float _range = 15f;
     public Transform _goal; // destination
     public Transform _player;
+    public Transform _self;
     private Animator _anim;
     private bool _alive;
     private float _distance;
@@ -28,9 +29,15 @@ public class BystanderMovement : MonoBehaviour {
         {
             _agent.destination = _goal.position;
             _anim.SetBool("byStanderRunning", true);
-        }
-        else if (!_alive || _distance >= _range) 
-            _anim.SetBool("byStanderRunning", false);
 
+            // if the bystander reaches its target, return to the idle position
+            if(Vector3.Distance(_self.position, _goal.position) <= 1.0f) {
+                _anim.SetBool("byStanderRunning", false);
+            }
+        }
+        else if (!_alive || _distance >= _range) {
+            _anim.SetBool("byStanderRunning", false);
+            _agent.destination = _self.position;
+        }
     }
 }
