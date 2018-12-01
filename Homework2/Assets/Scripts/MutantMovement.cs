0be2956325b1ad5a2d7445f9df3a41bdf5069fa5
Vrezh Khalatyan <vkhalat@calstatela.edu>
2018-11-30 21:26:39 -0800

    Added Player Jump/ & Moving Objects

--- Homework2/Assets/Scripts/MutantMovement.cs
+++ Homework2/Assets/Scripts/MutantMovement.cs
@@ -7,7 +7,7 @@ using UnityEngine.AI;
 The mutant has a angle of sight and sense when the player is close by.
 The NPC walks, runs, attacks, and jumps at a certain distance between the 
 player and the NPC*/
-[RequireComponent(typeof(NavMeshAgent))]
+
 [RequireComponent(typeof(Animator))]
 public class MutantMovement : MonoBehaviour
 {
@@ -18,17 +18,14 @@ public class MutantMovement : MonoBehaviour
     private Animator _anim;
     private bool _alive;
     private float _distance;
-    private NavMeshAgent _agent;
     private AudioSource _attackSound;
 
-    private Transform _Off_Mesh_Platform;
-    float runSpeed = 2f;
 
     void Start()
     {
         _alive = true;
         // get a reference to the NavMesh Agent component
-        _agent = GetComponent<NavMeshAgent>();
+        
         _player = GameObject.Find("Player").transform;
         _anim = GetComponent<Animator>();
         // _anim.SetBool("byStanderRunning", true);
@@ -39,39 +36,25 @@ public class MutantMovement : MonoBehaviour
 
     void Update()
     {
-        _distance = Vector3.Distance(this.transform.position, _player.transform.position);
+        
         Vector3 direction = _player.position - this.transform.position;
         float angle = Vector3.Angle(direction, this.transform.forward);
         if(Vector3.Distance(_player.position, this.transform.position) < 10 && angle < 360){
             _anim.SetBool("isIdle", false);
+            transform.LookAt(_player);
             if(_alive && direction.magnitude > 5){
-                _agent.destination = _player.position;
+                //this.transform.Translate(0, 0, 0.05f);
                 _anim.SetBool("isWalking", true);
                 _anim.SetBool("isRunning", false);
-                _anim.SetBool("isAttacking", false);
                 if(_alive && direction.magnitude > 6.5){
-                    _agent.destination = _player.position;
-                    _agent.speed = runSpeed;
+                    //this.transform.Translate(0, 0, 0.7f);
                     _anim.SetBool("isRunning", true);
                     _anim.SetBool("isWalking", false);
-                    _anim.SetBool("isAttacking", false);
                 }
-            }else{
-                _anim.SetBool("isAttacking", true);
-                _attackSound.Play();
-                _anim.SetBool("isWalking", false);
-                _anim.SetBool("isRunning", false);
             }
         }else{
             _anim.SetBool("isIdle", true);
             _anim.SetBool("isWalking", false);
-            _anim.SetBool("isAttacking", false);
-        }
-        if(_Off_Mesh_Platform.position == this.transform.position){
-            _anim.SetBool("isJumping", true);
-            _anim.SetBool("isWalking", false);
-            _anim.SetBool("isRunning", false);
-            _anim.SetBool("isAttacking", false);
         }
     }
 
